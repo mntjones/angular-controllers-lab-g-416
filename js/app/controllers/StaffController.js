3fed9ebfb2b630aea2408d3c1af5a14f90886a55
Monica Tamaru Jones <mntamaru@gmail.com>
2019-10-22 19:42:09 +0000

    Automatically backed up by Learn

--- js/app/controllers/StaffController.js
+++ js/app/controllers/StaffController.js
@@ -1,6 +1,6 @@
 function StaffController($scope) {
-    $scope.name = 'Steve Jobs';
-    $scope.email = 'steve@apple.com';
+    this.name = 'Steve Jobs';
+    this.email = 'steve@apple.com';
 }
 
 angular
